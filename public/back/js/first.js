56f076bb38be83b0ed337b0a747b6fa5d68d16e8
hsl <hsl221978@163.com>
2018-11-22 16:28:53 +0800

    一级分类完成

--- public/back/js/first.js
+++ public/back/js/first.js
@@ -47,5 +47,68 @@ var pageSize = 5; //每页条数
  }
   
 
+//  点击分类按钮显示模态框
+$('#addBtn').click(function(){
+  // 模态框展示
+  $('#firstModal').modal("show");
+
+})
+
+// 表单校验
+// $('#firstAddBtn').click(function(){
+  $('#form').bootstrapValidator({
+    //  校验图标显示
+     feedbackIcons: {
+        valid: 'glyphicon glyphicon-ok',
+        invalid: 'glyphicon glyphicon-remove',
+        validating: 'glyphicon glyphicon-refresh'
+      },
+    // 配置校验字段
+    fields:{
+      categoryName:{
+        // 配置校验规则
+        validators:{
+          //配置非空校验
+          notEmpty:{
+            message:"请输入一级分类名称",
+          }
+        }
+      }
+    }
+  });
+  
+
+// 注册表单校验成功事件，阻止默认的表单的提交，通过ajax提交
+
+  $('#form').on("success.form.bv",function(e){
+
+    // 阻止默认表单的提交
+    e.preventDefault();
+    // 发送ajax请求,添加成功后重新渲染页面
+    $.ajax({
+       type:'post',
+       url:'/category/addTopCategory',
+       data:$('#form').serialize(),
+       dataTpe:'json',
+       success:function(info){
+        //  console.log(info);
+        if(info.success){
+          // 添加成功
+
+          // 关闭模态框
+          $('#firstModal').modal("hide");
+          // 重新渲染页面
+          // 重新渲染第一页
+          currentPage = 1;
+          render();
+          // 内容和状态都要重置
+          $('#form').data("bootstrapValidator").resetForm(true);
+        }
+       }
+     })
+  })
+
+// })
+
 
 })
